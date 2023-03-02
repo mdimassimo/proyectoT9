5cb8a194a8cabc07f5e042a9b9a7ddfb912db387
Miguel Di Massimo <miguel.dimassimo@gmail.com>
2023-03-02 18:10:09 +0100

    Creación de documentación

---
+++ doc/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\listadoPersonajes\u0028\u0029",
+            "name": "listadoPersonajes",
+            "summary": "Esta\u0020funci\u00F3n\u0020se\u0020encarga\u0020de\u0020consultar\u0020a\u0020la\u0020API\u0020todo\u0020el\u0020listado\u0020de\u0020personajes\u0020disponibles",
+            "url": "namespaces/default.html#function_listadoPersonajes"
+        },                {
+            "fqsen": "\\obtenerPersonajes\u0028\u0029",
+            "name": "obtenerPersonajes",
+            "summary": "Esta\u0020funci\u00F3n\u0020se\u0020encarga\u0020de\u0020consultar\u0020a\u0020la\u0020API\u0020la\u0020informaci\u00F3n\u0020de\u0020un\u0020personaje\u0020en\u0020concreto,\u0020en\u0020base\u0020a\u0020su\u0020id",
+            "url": "namespaces/default.html#function_obtenerPersonajes"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
